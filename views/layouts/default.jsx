8076b758e68b94805872bff0a74776ad4b26f8cd
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-08-13 12:04:57 -0400

    sending index and show data

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -4,13 +4,13 @@ function Default (html) {
     return (
         <html>
             <head>
-                <title>Default</title>
+                <title>{html.title || 'Default'}</title>
             </head>
             <body>
                 <h1>HTML Rendered!</h1>
-                <div className="container">
+                {/*<div className="container">*/}
                     {html.children}
-                </div>
+                {/*</div>*/}
             </body>
         </html>
     )
