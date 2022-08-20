d9be4398d16e0630cd56fdfd51f4803b8c9ba9c5
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-08-20 12:56:24 -0400

    fixed screw up

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,9 +4,11 @@ const Bread = require("../models/bread.js");
 
 //INDEX
 breads.get("/", (req, res) => {
-  res.render("Index", {
-    breads: Bread,
-    title: "Index Page",
+  Bread.find().then((foundBread) => {
+    res.render("Index", {
+      bread: foundBread,
+      title: "Index Page",
+    });
   });
   // res.send(Bread);
 });
@@ -14,6 +16,14 @@ breads.get("/", (req, res) => {
 // NEW
 breads.get("/new", (req, res) => {
   res.render("new");
+});
+
+// EDIT
+breads.get("/:indexArray/edit", (req, res) => {
+  res.render("edit", {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray,
+  });
 });
 
 //SHOW
@@ -43,15 +53,14 @@ breads.get("/:arrayIndex", (req, res) => {
 // CREATE
 breads.post("/", (req, res) => {
   if (!req.body.image) {
-    req.body.image =
-      "https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
+    req.body.image = undefined;
   }
   if (req.body.hasGluten === "on") {
     req.body.hasGluten = true;
   } else {
     req.body.hasGluten = false;
   }
-  Bread.push(req.body);
+  Bread.create(req.body);
   res.redirect("/breads");
 });
 
