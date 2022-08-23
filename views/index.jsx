4800cf346bac73c7da7f5748afec8fff953ac82c
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-08-23 01:40:43 -0400

    create and new routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -5,6 +5,10 @@ function Index ({breads, title}) {
     return (
       <Default title={title}>
         <h2>Index Page</h2>
+        <div className="newButton">
+            <a href="/breads/new"><button>Add a new bread</button></a>
+        </div>
+
         {/* This is a JSX comment. */}
         {/* <p>I have {breads[0].name} bread!</p> */}
         <ul>
