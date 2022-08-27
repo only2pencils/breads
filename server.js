4e46bbe4c33712acc5ae90a03d3e866db4921cb8
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-08-27 01:33:48 -0400

    create and new routes complete

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.static("public"));
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 // app.use(methodOverride("_method"));
 
 // // ROUTES
