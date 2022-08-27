be3987c0244e43f7127788a9501136cff2247a07
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-08-27 01:08:16 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
-// app.use(express.static("public"));
+app.use(express.static("public"));
 // app.use(express.urlencoded({ extended: true }));
 // app.use(methodOverride("_method"));
 
@@ -32,10 +32,10 @@ app.get("/", (req, res) => {
 const breadsController = require("./controllers/breads_controller.js");
 app.use("/breads", breadsController);
 
-// // 404 Page
-// app.get("*", (req, res) => {
-//   res.send("404");
-// });
+// 404 Page
+app.get("*", (req, res) => {
+  res.send("404");
+});
 
 // // LISTEN
 app.listen(PORT, () => {
