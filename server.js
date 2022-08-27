17f4313c6180518633e091c1318574d16fbc313f
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-08-27 00:30:14 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const app = express();
 // );
 
 // // MIDDLEWARE
-// app.set("views", __dirname + "/views");
-// app.set("view engine", "jsx");
-// app.engine("jsx", require("express-react-views").createEngine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
 // app.use(express.static("public"));
 // app.use(express.urlencoded({ extended: true }));
 // app.use(methodOverride("_method"));
--- views/index.jsx
+++ views/index.jsx
@@ -1,5 +1,29 @@
-// const React = require('react')
-// const Default = require('./layouts/Default')
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function Index ({breads}) {
+    return (
+      <Default>
+        <h2>Index Page</h2>
+        {/* This is a JSX comment. */}
+        {/* <p>I have {breads[0].name} bread!</p> */}
+        <ul>
+          {
+            breads.map((bread, index)=> {
+                return (<li key={index}>
+                    <a href={`/breads/${index}`}>
+                        {bread.name}
+                    </a>
+                </li>)
+                })
+          }
+        </ul>
+      </Default>
+    )
+}
+
+module.exports = Index
+
 
 // function Index ({breads, title}) {
 //     return (
